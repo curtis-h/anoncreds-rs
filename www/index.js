6436051594479a873ccfd8d11cecb6c3f1a1b2b1
Curtish <ch@curtish.me>
2023-12-08 10:29:19 +0000

    wip

--- www/index.js
+++ www/index.js
@@ -1,10 +1,13 @@
-import * as anoncreds from "wasm-pkg";
-import wasm from "wasm-pkg/anoncreds_bg.wasm";
+// import * as anoncreds from "wasm-pkg";
+// import wasm from "wasm-pkg/anoncreds_bg.wasm";
+import * as anoncreds from "../pkg";
+import wasm from "../pkg/anoncreds_bg.wasm";
 
 (async () => {
   await anoncreds.default(wasm());
-
+  
   window.anoncreds = anoncreds;
   window.wasm = anoncreds;
-  console.log("READY");
+  anoncreds.anoncredsSetDefaultLogger();
+  // console.log("READY");
 })()
